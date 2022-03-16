752fce720a3616a47c4c6589cfb778e0bf45a550
Hein Aung <heinaung@Heins-MacBook-Pro.local>
2022-03-16 22:59:09 +0800

    added feature testing for api

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,49 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_api_order_post(){
+        $response = $this->json('POST', 'api/order', array(
+            "aTZg88qFyXYd1e5XEQUqt1" => [
+                "timestamp" => "2022-01-27 14:35:08",
+                "status" => "processing"
+            ]
+        ))->assertJsonStructure([
+            "aTZg88qFyXYd1e5XEQUqt1" => [
+                'timestamp',
+                'status',
+                'order_code',
+                'id'
+                ]
+            ]);
+    }
+
+    public function test_api_order_get(){
+        $this->json('GET', 'api/order/aTZg88qFyXYd1e5XEQUqt1')
+            ->assertJsonStructure([
+                "aTZg88qFyXYd1e5XEQUqt1" => [
+                    'timestamp',
+                    'status',
+                    'order_code',
+                    'id'
+                    ]
+                ]);
+    }
+
+    public function test_api_order_get_with_timestamp(){
+        $this->json('GET', 'api/order/get_all_records')
+            ->assertJsonStructure([
+                [
+                    'order_code',
+                    'deliveries' => [
+                        [
+                            'id',
+                            'order_code',
+                            'timestamp',
+                            'status'
+                        ]
+                    ]
+                ]
+                ]);
+    }
 }
